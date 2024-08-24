c987b12f1a649b0ced52b11207c8a85d3a7037ae
Thalita Meira <thalita_meira@hotmail.com>
2024-08-24 20:32:46 -0300

    Add project files.

---
+++ Banking/Interfaces/IConta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Interfaces
+{
+    public interface IConta
+    {
+        public string Name { get; set; }
+        public int Id {  get; set; }
+        public decimal Saldo { get; set; }
+
+
+    }
+}
